a7dca1aaac1bb4f1f54a6478bde385eeb1ce26db
ERRORthenBSOD <atmobsod@gmail.com>
2019-08-11 14:01:44 +0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-nav tabs>
+    <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
+    <b-nav-item disabled>Counter {{numberCorrect}}/{{numberTotal}}</b-nav-item>
+  </b-nav>
+</template>
+
+<script>
+export default {
+  props: [
+    'numberCorrect',
+    'numberTotal',
+  ]
+}
+</script>
